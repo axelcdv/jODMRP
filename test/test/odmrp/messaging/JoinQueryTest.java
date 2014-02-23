a8278f2e3c0e1f01fb1bba5a1fe94364ef88404d
Axel Colin de Verdiere <axel@axelcdv.com>
2014-02-23 02:20:39 +0100

    Test for JoinQuery class

--- test/test/odmrp/messaging/JoinQueryTest.java
+++ test/test/odmrp/messaging/JoinQueryTest.java
@@ -1,22 +1,25 @@
 package test.odmrp.messaging;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
 
 import java.net.Inet6Address;
 import java.net.UnknownHostException;
 
 import net.odmrp.constants.Constants;
+import net.odmrp.messaging.JoinQuery;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class PacketTest {
-	
+public class JoinQueryTest {
+
 	private static Inet6Address _sourceAddress;
 	private static Inet6Address _groupAddress;
+	private static int _sequenceNumber;
 
 	@BeforeClass
-	public static void setup() {
+	public static void setUpBeforeClass() throws Exception {
 		try {
 			byte[] sourceBytes = new byte[16];
 			sourceBytes[15] = 1;
@@ -29,6 +32,8 @@ public class PacketTest {
 			_groupAddress = Inet6Address.getByAddress("group",
 					groupBytes,
 					Constants.DEFAULT_IPV6_SCOPE);
+			
+			_sequenceNumber = 12;
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -36,9 +41,17 @@ public class PacketTest {
 	}
 
 	@Test
-	public void test() {
-//		fail("Not yet implemented");
-		assertTrue(true);
+	public void encodeDecode() {
+		try {
+			JoinQuery joinQuery = new JoinQuery(_sourceAddress, _groupAddress, _sequenceNumber);
+			byte[] jqBytes = joinQuery.toBytes();
+			assertArrayEquals("Encoding then decoding should give back the same JQ",
+					jqBytes,
+					new JoinQuery(jqBytes, 0).toBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("error: " + e);
+		}
 	}
 
 }
---
+++ test/test/odmrp/messaging/MessagingTests.java
@@ -0,0 +1,11 @@
+package test.odmrp.messaging;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ JoinQueryTest.class, PacketTest.class })
+public class MessagingTests {
+
+}
